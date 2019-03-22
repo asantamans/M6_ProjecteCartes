573927669921e16a85fdf26f6009e116cb58e405
Arnau <arnau.santamans@gmail.com>
2019-03-22 19:48:39 +0100

    Actualizacion IBaralla

--- src/main/java/idao/IBaralla.java
+++ src/main/java/idao/IBaralla.java
@@ -5,4 +5,5 @@ import Objectes.Baralla;
 public interface IBaralla {
 	public Baralla getDeckFromName(String nom);
 	public boolean guardarBaralla(Baralla b1);
+	public boolean actualitzarBaralla(Baralla b1);
 }
